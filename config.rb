8d348d4bba71f992d0c79db9a75bce988d50b17b
Ryan Tvenge <ryan@tvenge.me>
2012-09-27 09:36:36 -0500

    Changed orginzation (2nd try)

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
-output_style = :nested
+output_style = :compressed
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
