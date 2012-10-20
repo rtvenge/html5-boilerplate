a624662f8615bdab7e3198347272fa5a6f232f98
Ryan Tvenge <ryan@tvenge.me>
2012-10-20 09:34:18 -0500

    More alterations/optimizations
    * Added global .gitignore_global file with common ignores
    * removed formee.js. It's worthless
    * Added drop-menus (from ryantvenge.com)
    * html5shiv is now default. Have to opt into modernizer
    * moved query to top of page (that's what wordpress does so let's
    conform. Plus if your site ever has jQuery in the content area for some
    reason, you are covered.
    * Added Variables for wells and grids in scss.
    * cleaned up random things in scss.
    * removed chrome frame from scss and html
    * Added variables for responsive breakpoints.
    * Added optional date styles in wordpress.scss
    * Added some empty scss files to hint at organizing blocks of the
    design in different files (widgets, content).

---
+++ js/drop-menu.js
@@ -0,0 +1,39 @@
+(function () {
+	$('.nav').each(function () {
+		var
+			nav = $(this),
+			menus = nav.find('ul, div'),
+			links = nav.find('a');
+		// Keeps parent anchors highlighted while hovering menus
+		menus.hover(
+			function () {
+				$(this)
+					.parent('li')
+					.find('> a')
+						.addClass('highlight');
+			},
+			function () {
+				$(this)
+					.parent('li')
+					.find('> a')
+						.removeClass('highlight');
+			}
+		);
+		// code to get tabbing to work properly
+		links
+			.blur(function () {
+				$(this).parent('li').removeClass('hover');
+			})
+			.focus(function () {
+				$(this).parents('li').addClass('hover');
+			});
+		// more tabbing code
+		nav.find('li:has(ul,div) a:last-child')
+			.blur(function () {
+				$(this).parents('li').removeClass('hover');
+			});
+		//signals menus with children
+		nav.find('li:has(ul,div)').addClass('children');
+
+	});
+})();
