d58c9ceaf14f8032412915d5e71df8a83ead759e
Ryan Tvenge <ryan@tvenge.me>
2013-02-13 21:52:39 -0600

    updating js files
    Signed-off-by: Ryan Tvenge <ryan@tvenge.me>

--- js/plugins.js
+++ js/plugins.js
@@ -1,19 +1,25 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var noop = function noop() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = window.console || {};
+	var
+		noop = function noop() {},
+		methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeStamp', 'trace', 'warn'
+		],
+		length = methods.length,
+		console = window.console || {};
 
-    while (length--) {
-        // Only stub undefined methods.
-        console[methods[length]] = console[methods[length]] || noop;
-    }
+	while (length--) {
+		// Only stub undefined methods.
+		console[methods[length]] = console[methods[length]] || noop;
+	}
 }());
 
 // Place any jQuery/helper plugins in here.
+
+//if you aren't using modernizr. remove no-js class from <html>
+(function () {
+	$('html:first').removeClass('no-js');
+}());
